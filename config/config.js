b97b2e1cb8dc1484840c132fc767c5f8ada53327
Obed <obejuela4@gmail.com>
2019-07-28 13:58:05 -0500

    Add config DB enviroment

--- config/config.js
+++ config/config.js
@@ -1,9 +1,22 @@
 // ============================
 //  PORT
 // ============================
-process.env.PORT = process.env.PORT || 5000;
+process.env.PORT = process.env.PORT || 5000
+
+
+// ============================
+//  ENVIROMENT
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV ||'dev'
 
 // ============================
 //  DB
 // ============================
-process.env.URLDB = 'mongodb://localhost:27017/holidays';
+let urlDB
+if(process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/holidays'
+} else {
+    urlDB = process.env.MLABDB
+}
+
+process.env.URLDB = urlDB
